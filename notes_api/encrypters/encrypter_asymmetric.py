e0c8549d084f6dcb89e69e51723c511aa5d82e4a
giornos <sgiorno@LFR034707.corp.capgemini.com>
2020-01-17 16:55:12 +0100

    Comments out some unimplemented functions

--- notes_api/encrypters/encrypter_asymmetric.py
+++ notes_api/encrypters/encrypter_asymmetric.py
@@ -29,18 +29,18 @@ class EncrypterAsymmetric(Encrypter):
         self._box = Box(self.private_key, self._public_key)
 
 
-    @classmethod
-    def from_file(cls, file_name, password=None):
-        """
-        Create an EncrypterAsymetric object from a private key serialized file.
-        """
-        with open(private_key, "rb") as key_file:
-            private_key = serialization.load_pem_private_key(
-                key_file.read(),
-                password=password,
-                backend=default_backend()
-            )
-        return EncrypterAsymmetric(private_key)
+    # @classmethod
+    # def from_file(cls, file_name, password=None):
+    #     """
+    #     Create an EncrypterAsymetric object from a private key serialized file.
+    #     """
+    #     with open(private_key, "rb") as key_file:
+    #         private_key = serialization.load_pem_private_key(
+    #             key_file.read(),
+    #             password=password,
+    #             backend=default_backend()
+    #         )
+    #     return EncrypterAsymmetric(private_key)
 
     def encrypt(self, clear_message):
         encrypted = self._box.encrypt(clear_message.encode())
@@ -50,21 +50,21 @@ class EncrypterAsymmetric(Encrypter):
         decrypted = self._box.decrypt(cipher_message).decode()
         return decrypted
 
-    def save_private_key(self, file_name):
-        pem = self.key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.NoEncryption()
-        )
-        with open(file_name, "wb") as key_file:
-            key_file.write(pem)
+    # def save_private_key(self, file_name):
+    #     pem = self.key.private_bytes(
+    #         encoding=serialization.Encoding.PEM,
+    #         format=serialization.PrivateFormat.TraditionalOpenSSL,
+    #         encryption_algorithm=serialization.NoEncryption()
+    #     )
+    #     with open(file_name, "wb") as key_file:
+    #         key_file.write(pem)
 
-    def save_public_key(self, file_name):
-        public_key = self.key.public_key()
-        pem = public_key.public_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo,
-        )
-        with open(file_name, "wb") as key_file:
-            key_file.write(pem)
+    # def save_public_key(self, file_name):
+    #     public_key = self.key.public_key()
+    #     pem = public_key.public_bytes(
+    #         encoding=serialization.Encoding.PEM,
+    #         format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    #     )
+    #     with open(file_name, "wb") as key_file:
+    #         key_file.write(pem)
 
